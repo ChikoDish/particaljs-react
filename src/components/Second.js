93d24a17d5f531c596ece65025a41e9f4cb759ea
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-03-06 14:50:26 +0530

    added first particle

---
+++ src/components/Second.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Particles from "react-particles-js";
+
+const Second = () => {
+  return (
+    <div>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 50,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default Second;
