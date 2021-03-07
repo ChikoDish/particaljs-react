bd1922807add4bd9201e8c6f2fe8bcc68a16f9b2
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-03-07 06:33:20 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
+import Particles from "react-particles-js";
 import First from "./components/First";
 import Second from "./components/Second";
+import NightSky from "./components/NightSky";
+import Snow from "./components/Snow";
+import Polygon from "./components/Polygon";
 function App() {
   return (
-    <div className="App">
-      <First />
-    </div>
+    <Router>
+      <NavLink exact to="/snow">
+        Snow
+      </NavLink>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink exact to="/second">
+        Second
+      </NavLink>
+      <NavLink exact to="/night">
+        NightSky
+      </NavLink>
+      <NavLink exact to="/polygon">
+        Polygon
+      </NavLink>
+      <Route path="/snow">
+        <Snow />
+      </Route>
+      <Route path="/">
+        <First />
+      </Route>
+      <Route path="/second">
+        <Second />
+      </Route>
+      <Route path="/night">
+        <NightSky />
+      </Route>
+      <Route path="/polygon">
+        <Polygon />
+      </Route>
+    </Router>
   );
 }
 
---
+++ src/components/Snow.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Particles from "react-particles-js";
+
+const Snow = () => {
+  return (
+    <div>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: false,
+              },
+            },
+            size: {
+              value: 10,
+              random: true,
+            },
+            move: {
+              direction: "bottom",
+              out_mode: "out",
+            },
+            line_linked: {
+              enable: false,
+            },
+          },
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "remove",
+              },
+            },
+            modes: {
+              remove: {
+                particles_nb: 10,
+              },
+            },
+          },
+        }}
+      />
+      v3.3.0
+    </div>
+  );
+};
+
+export default Snow;
